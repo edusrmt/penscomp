bbe4443568e60d9274a35c4e8cba7eca13d0cb4d
Eduardo Sarmento <cealvesarmento@gmail.com>
2019-06-10 23:02:13 -0300

    Telas iniciais criadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,22 +11,43 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+
 import { CourseListComponent } from './course-list/course-list.component';
+import { SubjectCardComponent } from './subject-card/subject-card.component';
+import { PanelComponent } from './panel/panel.component';
+import { LoginComponent } from './login/login.component';
+import { SubjectListComponent } from './subject-list/subject-list.component';
+import { LessonComponent } from './lesson/lesson.component';
+
+const appRoutes : Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'panel', component: PanelComponent},
+    {path: 'subject', component: SubjectListComponent},
+    {path: '', redirectTo: '/login', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    CourseListComponent
+    CourseListComponent,
+    SubjectListComponent,
+    SubjectCardComponent,
+    PanelComponent,
+    LoginComponent,
+    LessonComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSidenavModule,
     MatButtonModule,
     MatGridListModule,
-    MatCardModule
+    MatCardModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/subject-card/subject-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subject-card',
+  templateUrl: './subject-card.component.html',
+  styleUrls: ['./subject-card.component.css']
+})
+export class SubjectCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
