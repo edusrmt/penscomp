33059d6af7051ccb5278d017cfe3fed03349aef7
Iury Lamonie <iury.lamonie@hotmail.com>
2019-11-15 08:32:05 -0300

    Caminho da CSS.

--- Application/views/professor/tarefas/master.php
+++ Application/views/professor/tarefas/master.php
@@ -4,8 +4,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>PENSCOMP</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-   <link rel="stylesheet" type="text/css" media="screen" href="./../../../../css/style.css" />
-    <link rel="stylesheet" type="text/css" media="screen" href="./../../../../css/viewer.css" />
+   <link rel="stylesheet" type="text/css" media="screen" href="./../../../../public/assets/css/style.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="./../../../../public/assets/css/viewer.css" />
 </head>
 <body>
 <header>
